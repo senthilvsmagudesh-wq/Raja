758cb175ac8b4255a2c4daee06e71ffe630c4efe
SenthilkumaranPSK <your-email@example.com>
2025-10-30 22:25:28 +0530

    Simplify vite.config.ts paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@": "./client/src",
+      "@shared": "./shared",
+      "@assets": "./attached_assets",
     },
   },
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: "./dist/public",
     emptyOutDir: true,
     rollupOptions: {
       input: 'client/index.html', // Explicitly specify the entry point
