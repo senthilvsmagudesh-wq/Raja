ac5cae8fb21af2eeb3b62716b55865a91176a927
SenthilkumaranPSK <your-email@example.com>
2025-10-30 20:39:03 +0530

    Specify index.html as entry point in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "client/index.html"),
+    },
   },
   server: {
     fs: {
